deff5535f641d5ff4ce0434fa811cedf11e65a5d
ChloeLee01 <youngsunyi01@gmail.com>
2024-12-06 17:04:03 +0900

    swagger.yaml

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const authRouter = require("./routes/authRoutes");
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const app = express();
 
@@ -11,6 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/upload", express.static(path.join(__dirname, "upload"))); // 이미지 경로가 단순 문자열이 아닌 실제 경로 형태로 변경해 주는 미들웨어 코드
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.send("auth server test running");
